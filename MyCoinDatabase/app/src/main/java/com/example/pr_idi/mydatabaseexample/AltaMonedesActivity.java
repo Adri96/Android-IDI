9d23fa276042edf93f334f07ad1252f683b3defa
Adri96 <adriamunuera@me.com>
2017-06-03 22:30:14 +0200

    Humushukula

---
+++ MyCoinDatabase/app/src/main/java/com/example/pr_idi/mydatabaseexample/AltaMonedesActivity.java
@@ -0,0 +1,13 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AltaMonedesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_monedes);
+    }
+}
